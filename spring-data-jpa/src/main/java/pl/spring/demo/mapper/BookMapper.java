566231c207eae6bc6d95e80fd29a0e0a30abd7b3
Kamil Świstowski <kamil.swistowski@capgemini.com>
2016-01-16 16:56:14 +0100

    BookTo & BookEntity mapper

---
+++ spring-data-jpa/src/main/java/pl/spring/demo/mapper/BookMapper.java
@@ -0,0 +1,44 @@
+package pl.spring.demo.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.spring.demo.entity.BookEntity;
+import pl.spring.demo.to.AuthorTo;
+import pl.spring.demo.to.BookTo;
+
+public class BookMapper {
+	public static BookTo map(BookEntity bookEntity) throws NullPointerException {
+		if(bookEntity == null)
+			throw new NullPointerException();
+		StringBuilder sb = new StringBuilder();
+		for(AuthorTo author : bookEntity.getAuthors()) {
+			sb.append(author);
+		}
+		
+		return new BookTo(bookEntity.getId(), bookEntity.getTitle(), sb.toString());
+	}
+	
+	public static BookEntity map(BookTo bookTo) throws NullPointerException {
+		if(bookTo == null)
+			throw new NullPointerException();
+		
+		return new BookEntity(bookTo.getId(), bookTo.getTitle(), bookTo.getAuthors());
+	}
+	
+	public static List<BookTo> map2To(List<BookEntity> bookEntities) {
+		List<BookTo> listBookTows = new ArrayList<BookTo>();
+		for(BookEntity bookEntity : bookEntities) {
+			listBookTows.add(BookMapper.map(bookEntity));
+		}
+        return listBookTows;
+    }
+
+    public static List<BookEntity> map2Entity(List<BookTo> bookTows) {
+    	List<BookEntity> listBookEntities = new ArrayList<BookEntity>();
+		for(BookTo bookTo : bookTows) {
+			listBookEntities.add(BookMapper.map(bookTo));
+		}
+        return listBookEntities;
+    }
+}
